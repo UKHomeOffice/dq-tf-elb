ae074ea0dea484ab2562a68d95d9961ae8218955
david-wirelab <info@wirelab.co.uk>
2018-01-29 16:22:31 +0000

    removed autoscaling config, added new tests and variables

--- main.tf
+++ main.tf
@@ -1,25 +1,18 @@
-resource "aws_autoscaling_group" "app" {
-  vpc_zone_identifier  = ["${var.subnet_list}"]
-  min_size             = "${var.asg_min}"
-  max_size             = "${var.asg_max}"
-  launch_configuration = "${aws_launch_configuration.app.id}"
-  target_group_arns    = ["${aws_alb_target_group.app.arn}"]
+resource "aws_alb" "app" {
+  subnets            = ["${var.subnet_list}"]
+  security_groups    = ["${var.security_groups}"]
+  load_balancer_type = "${var.lb_type}"
+  internal           = true
 }
 
-resource "aws_launch_configuration" "app" {
-  security_groups = [
-    "${var.security_groups}",
-  ]
+resource "aws_alb_listener" "app" {
+  load_balancer_arn = "${aws_alb.app.id}"
+  port              = "${var.target_group_port}"
+  protocol          = "${var.protocol_type}"
 
-  key_name                    = "${var.key_name}"
-  image_id                    = "${var.ami_id}"
-  instance_type               = "${var.instance_type}"
-  iam_instance_profile        = "${var.instance_profile}"
-  user_data                   = "${var.user_data}"
-  associate_public_ip_address = false
-
-  lifecycle {
-    create_before_destroy = true
+  default_action {
+    target_group_arn = "${aws_alb_target_group.app.id}"
+    type             = "forward"
   }
 }
 
@@ -29,18 +22,10 @@ resource "aws_alb_target_group" "app" {
   vpc_id   = "${var.vpc_id}"
 }
 
-resource "aws_alb" "app" {
-  subnets         = ["${var.subnet_list}"]
-  security_groups = ["${var.security_groups}"]
-}
-
-resource "aws_alb_listener" "front_end" {
-  load_balancer_arn = "${aws_alb.app.id}"
-  port              = "${var.target_group_port}"
-  protocol          = "${var.protocol_type}"
-
-  default_action {
-    target_group_arn = "${aws_alb_target_group.app.id}"
-    type             = "forward"
-  }
+resource "aws_autoscaling_group" "app" {
+  vpc_zone_identifier  = ["${var.subnet_list}"]
+  min_size             = "${var.asg_min}"
+  max_size             = "${var.asg_max}"
+  launch_configuration = "${var.launch_config}"
+  target_group_arns    = ["${aws_alb_target_group.app.arn}"]
 }
