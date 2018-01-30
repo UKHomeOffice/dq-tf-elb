aa22ddc887110d3cdcd710804f617f38b6102430
david-wirelab <info@wirelab.co.uk>
2018-01-30 10:11:37 +0000

    added multi port support

--- main.tf
+++ main.tf
@@ -1,25 +1,27 @@
 resource "aws_alb" "app" {
   subnets            = ["${var.subnet_list}"]
   security_groups    = ["${var.security_groups}"]
-  load_balancer_type = "${var.lb_type}"
+  load_balancer_type = "network"
   internal           = true
 }
 
-resource "aws_alb_listener" "app" {
+resource "aws_alb_listener" "TCP" {
   load_balancer_arn = "${aws_alb.app.id}"
-  port              = "${var.target_group_port}"
-  protocol          = "${var.protocol_type}"
+  port              = "${element(var.TCPPorts, count.index)}"
+  protocol          = "TCP"
+  count             = "${length(var.TCPPorts)}"
 
   default_action {
-    target_group_arn = "${aws_alb_target_group.app.id}"
+    target_group_arn = "${aws_alb_target_group.app.*.id}"
     type             = "forward"
   }
 }
 
 resource "aws_alb_target_group" "app" {
-  port     = "${var.target_group_port}"
-  protocol = "${var.protocol_type}"
+  port     = "${element(var.TCPPorts, count.index)}"
+  protocol = "TCP"
   vpc_id   = "${var.vpc_id}"
+  count    = "${length(var.TCPPorts)}"
 }
 
 resource "aws_autoscaling_group" "app" {
@@ -27,5 +29,5 @@ resource "aws_autoscaling_group" "app" {
   min_size             = "${var.asg_min}"
   max_size             = "${var.asg_max}"
   launch_configuration = "${var.launch_config}"
-  target_group_arns    = ["${aws_alb_target_group.app.arn}"]
+  target_group_arns    = ["${aws_alb_target_group.app.*.arn}"]
 }
